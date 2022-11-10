514f8759beb761ef438dc418d9af00927fcc39df
Gabrielle <gabrielle01.spence@gmail.com>
2022-11-09 20:33:51 -0800

    finished game besides extra bonus because huh?

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
 class Casino {
-    // Write code here
+    constructor(name, isFakeCoin){
+        this.name=name;
+        this.timesPlayed = 0;
+        this.isFakeCoin = isFakeCoin;
+    }
+    playGame(betAmount){
+        if(this.isFakeCoin||Math.random()<=0.5){
+            console.log(`${this.name} wins!`)
+        }
+        else{
+            console.log(`The player wins $${betAmount*(this.timesPlayed+1)}!`)
+
+        }
+        this.timesPlayed+=1;
+    }
+
 };
 
 // TESTS
-const myCasino = new Casino("HackerU Casino");
+const myCasino = new Casino("HackerU Casino", false);
 console.log(myCasino);
 myCasino.playGame(5);
 myCasino.playGame(15);
@@ -11,14 +26,14 @@ myCasino.playGame(25);
 myCasino.playGame(35);
 
 // BONUS TESTS
-/*
+
 const myBonusCasino = new Casino("HackerU Bonus Casino", true);
 console.log(myBonusCasino);
 myBonusCasino.playGame(5);
 myBonusCasino.playGame(15);
 myBonusCasino.playGame(25);
 myBonusCasino.playGame(35);
-*/
+
 
 // Extra BONUS TESTS
 /*
